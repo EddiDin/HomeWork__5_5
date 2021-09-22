0dba06618dedc392ed1cf09361fd733485ceea80
Kirill Sedov <demotest91@gmail.com>
2021-09-23 00:20:14 +0300

    master задание 5_5_2

--- HomeWork__5_5_2/Program.cs
+++ HomeWork__5_5_2/Program.cs
@@ -30,29 +30,47 @@ namespace HomeWork__5_5_2
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// Ввод текста через консоль
+        /// </summary>
+        /// <returns>Строка</returns>
         static string EnterText()
         {
+            Console.WriteLine("Введите произвольный текст:");
             string userInput = Console.ReadLine();
+
             return userInput;
         }
 
+        /// <summary>
+        /// Получение самого короткого слова из текста
+        /// </summary>
+        /// <param name="text">Строка</param>
+        /// <returns>Строка</returns>
         static string GetMinLengthWord(string text)
         {
             List<string> Words = GetWordsFromText(text);
             Words.Sort(CompareByLength);
 
-            return Words[0];
+            return Words.Count == 0 ? "" : Words[0];
         }
 
+        /// <summary>
+        /// Выделение самых длинных слов из текста
+        /// </summary>
+        /// <param name="text">Строка</param>
+        /// <returns>Коллекция слов</returns>
         static List<string> GetMaxLengthWords(string text)
         {
             List<string> Words = GetWordsFromText(text);
             Words.Sort(CompareByLength);
             Words.Reverse();
 
-            int maxLength = Words[0].Length;
             List<string> filteredWordsList = new List<string>();
 
+            if (Words.Count == 0) return filteredWordsList;
+
+            int maxLength = Words[0].Length;
             foreach (string word in Words)
             {
                 if (word.Length == maxLength) filteredWordsList.Add(word);
@@ -61,6 +79,11 @@ namespace HomeWork__5_5_2
             return filteredWordsList;
         }
 
+        /// <summary>
+        /// Получение слов из текста
+        /// </summary>
+        /// <param name="text">Строка</param>
+        /// <returns>Коллекция слов</returns>
         static List<string> GetWordsFromText(string text)
         {
             List<string> Words = new List<string>();
@@ -73,6 +96,23 @@ namespace HomeWork__5_5_2
             return Words;
         }
 
+        /// <summary>
+        /// Функция для сортировки слов в коллекции по длине
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        static int CompareByLength(string x, string y)
+        {
+            if (x.Length > y.Length) return 1;
+            if (x.Length < y.Length) return -1;
+            return 0;
+        }
+
+        /// <summary>
+        /// Вывод заголовка в консоль
+        /// </summary>
+        /// <param name="message">Заголовок</param>
         static void ShowHeader(string message)
         {
             string divider = "----------------------------------------------------";
@@ -81,13 +121,6 @@ namespace HomeWork__5_5_2
             Console.SetCursorPosition((Console.WindowWidth - divider.Length) / 2, Console.CursorTop);
             Console.WriteLine(divider);
             Console.WriteLine();
-        }
-
-        static int CompareByLength(string x, string y)
-        {
-            if (x.Length > y.Length) return 1;
-            if (x.Length < y.Length) return -1;
-            return 0;
         }
     }
 }
