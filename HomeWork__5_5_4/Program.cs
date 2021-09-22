10b473b62a1574b10db2b9da522359afca3ddf93
Kirill Sedov <demotest91@gmail.com>
2021-09-23 00:10:39 +0300

    master задание 5_5_4

--- HomeWork__5_5_4/Program.cs
+++ HomeWork__5_5_4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HomeWork__5_5_4
 {
@@ -8,15 +9,89 @@ namespace HomeWork__5_5_4
         {
             ShowHeader("Определение прогрессии");
 
+            int[] sequence = EnterSequence();
 
+            Console.WriteLine("Введенная последовательность:");
+            Console.WriteLine(string.Join(',', sequence));
+
+            string result = isProgression(sequence);
+            Console.WriteLine();
+            Console.WriteLine(result);
+
+            Console.ReadKey();
         }
 
-        static string isProgression(params int[] args) {
-            if (args.Length < 3) return "Невозможно определить является ли последовательность прогрессией. Слишком мало чисел";
+        /// <summary>
+        /// Ввод последовательности
+        /// </summary>
+        /// <returns>Массив чисел</returns>
+        static int[] EnterSequence()
+        {
+            Console.WriteLine("Введите последовательность чисел через пробел:");
+            string userInput = Console.ReadLine();
+            List<int> sequence = GetNumbersFromString(userInput);
 
-            return "";
+            return sequence.ToArray();
         }
 
+        /// <summary>
+        /// Получение чисел из строки (разделитель пробел)
+        /// </summary>
+        /// <param name="text">Произвольная строка</param>
+        /// <returns>Коллекция чисел</returns>
+        static List<int> GetNumbersFromString(string text)
+        {
+            List<int> numbers = new List<int>();
+            System.Text.RegularExpressions.MatchCollection matches = System.Text.RegularExpressions.Regex.Matches(text, @"([0-9-]+)");
+            foreach (System.Text.RegularExpressions.Match match in matches) numbers.Add(Int32.Parse(match.Value));
+
+            return numbers;
+        }
+
+        /// <summary>
+        /// Определяет, является ли последовательность переданных аргументов прогрессией
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns>Сообщение с результатом</returns>
+        static string isProgression(params int[] args)
+        {
+            if (args.Length < 3) return "Невозможно определить является ли последовательность прогрессией. Слишком мало чисел.";
+
+            int divider = args[1] / args[0];
+            bool isGeometry = true;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i == 0 || i == 1) continue;
+                if (args[i] / divider != args[i - 1])
+                {
+                    isGeometry = false;
+                    break;
+                }
+            }
+
+            if (isGeometry) return "Введенная последовательноть является геометрической.";
+
+            int difference = args[1] - args[0];
+            bool isArithmetic = true;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i == 0 || i == 1) continue;
+                if (args[i] - difference != args[i - 1])
+                {
+                    isArithmetic = false;
+                    break;
+                }
+            }
+
+            if (isArithmetic) return "Введенная последовательноть является арифметической.";
+
+            return "Введенная последовательность не является прогрессией";
+        }
+
+        /// <summary>
+        /// Вывод заголовка в консоль
+        /// </summary>
+        /// <param name="message">Заголовок</param>
         static void ShowHeader(string message)
         {
             string divider = "----------------------------------------------------";
