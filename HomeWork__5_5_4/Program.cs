dd8162a9057fc62601fc4cb6e8504f09ed4bb56f
Kirill Sedov <demotest91@gmail.com>
2021-09-22 22:49:25 +0300

    master Старт 4-ого задания

---
+++ HomeWork__5_5_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWork__5_5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowHeader("Определение прогрессии");
+
+
+        }
+
+        static string isProgression(params int[] args) {
+            if (args.Length < 3) return "Невозможно определить является ли последовательность прогрессией. Слишком мало чисел";
+
+            return "";
+        }
+
+        static void ShowHeader(string message)
+        {
+            string divider = "----------------------------------------------------";
+            Console.SetCursorPosition((Console.WindowWidth - message.Length) / 2, Console.CursorTop);
+            Console.WriteLine(message);
+            Console.SetCursorPosition((Console.WindowWidth - divider.Length) / 2, Console.CursorTop);
+            Console.WriteLine(divider);
+            Console.WriteLine();
+        }
+    }
+}
